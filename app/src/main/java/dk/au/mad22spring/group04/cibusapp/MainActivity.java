d6df29d4796987224b0cf7cb0a17f3c40e3f9cc9
Randi Mikkelsen Bech <au615785@uni.au.dk>
2022-04-21 11:31:58 +0200

    Add navigation bar

--- app/src/main/java/dk/au/mad22spring/group04/cibusapp/MainActivity.java
+++ app/src/main/java/dk/au/mad22spring/group04/cibusapp/MainActivity.java
@@ -6,10 +6,18 @@ import androidx.fragment.app.FragmentContainerView;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import dk.au.mad22spring.group04.cibusapp.ui.fragments.UserRecipesList.UserRecipesListFragment;
+
+//Navigation bar: https://www.geeksforgeeks.org/bottom-navigation-bar-in-android/
 
 public class MainActivity extends AppCompatActivity {
 
+    //UI Widgets
+    private BottomNavigationView bottomNavigationMenuView;
 
     // Fragments:
     private UserRecipesListFragment userRecipesListFragment;
@@ -24,12 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
         //view setup:
         userRecipeListContainer = findViewById(R.id.fragConViewUserRecipesList);
+        bottomNavigationMenuView = findViewById(R.id.bottomNavigationView);
 
-        if(savedInstanceState == null){
+        //https://stackoverflow.com/questions/67641594/bottomnavigation-view-onnavigationitemselectedlistener-is-deprecated
+        bottomNavigationMenuView.setOnClickListener(item ->{
+            switch (item.getId()){
+                case R.id.user:
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragConViewUserRecipesList, UserRecipesListFragment.newInstance())
+                            .commit();
+            }
+        });
+
+/*        if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragConViewUserRecipesList, UserRecipesListFragment.newInstance())
                     .commitNow();
 
-        }
+        }*/
     }
 }

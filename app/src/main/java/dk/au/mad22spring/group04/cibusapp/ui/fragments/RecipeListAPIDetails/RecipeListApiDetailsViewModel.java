4c79b88b12cfe69a6633abd981ce962c5f5e65ea
MacQuynh <macquynhtrang@hotmail.com>
2022-05-06 09:19:22 +0200

    add strings and cleanup pending intent

--- app/src/main/java/dk/au/mad22spring/group04/cibusapp/ui/fragments/RecipeListAPIDetails/RecipeListApiDetailsViewModel.java
+++ app/src/main/java/dk/au/mad22spring/group04/cibusapp/ui/fragments/RecipeListAPIDetails/RecipeListApiDetailsViewModel.java
@@ -3,16 +3,8 @@ package dk.au.mad22spring.group04.cibusapp.ui.fragments.RecipeListAPIDetails;
 import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
 
-import java.util.List;
-
-import dk.au.mad22spring.group04.cibusapp.model.Component;
-import dk.au.mad22spring.group04.cibusapp.model.DTOs.InstructionDTO;
-import dk.au.mad22spring.group04.cibusapp.model.DTOs.RecipeDTO;
-import dk.au.mad22spring.group04.cibusapp.model.Instruction;
 import dk.au.mad22spring.group04.cibusapp.model.Result;
-import dk.au.mad22spring.group04.cibusapp.model.Section;
 import dk.au.mad22spring.group04.cibusapp.model.repository.Repository;
 
 
